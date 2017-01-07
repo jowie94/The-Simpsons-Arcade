c31c788ca25b94a7e1c8ceeeadf7a35e52c2f3ab
Joel Lopez <jowie.jowie@gmail.com>
2017-01-07 12:18:06 +0100

    Add walk and walk up animations to Royd

--- The Simpsons Arcade/Royd.cpp
+++ The Simpsons Arcade/Royd.cpp
@@ -22,6 +22,32 @@ bool Royd::Start()
 
 	AddAnimation("idle", idle);
 
+	SpriteAnimation walk;
+	walk.frames.push_back({ { 16, 32, 20, 64 }, iPoint(0, 64) });
+	walk.frames.push_back({ { 64, 32, 25, 64 }, iPoint(4, 64) });
+	walk.frames.push_back({ { 112, 33, 32, 63 }, iPoint(6, 63) });
+	walk.frames.push_back({ { 160, 33, 24, 63 }, iPoint(3, 63) });
+	walk.frames.push_back({ { 208, 33, 23, 63 }, iPoint(0, 63) });
+	walk.frames.push_back({ { 256, 33, 27, 63 }, iPoint(3, 63) });
+	walk.frames.push_back({ { 304, 34, 32, 62 }, iPoint(8, 62) });
+	walk.frames.push_back({ { 352, 32, 24, 64 }, iPoint(3, 64) });
+	walk.speed = 0.2f;
+
+	AddAnimation("walk", walk);
+
+	SpriteAnimation walk_up;
+	walk_up.frames.push_back({ { 16, 114, 25, 62 }, iPoint(0, 62) });
+	walk_up.frames.push_back({ { 64, 114, 27, 62 }, iPoint(0, 62) });
+	walk_up.frames.push_back({ { 112, 114, 32, 62 }, iPoint(1, 62) });
+	walk_up.frames.push_back({ { 160, 116, 27, 60 }, iPoint(0, 60) });
+	walk_up.frames.push_back({ { 208, 115, 26, 61 }, iPoint(0, 61) });
+	walk_up.frames.push_back({ { 256, 114, 28, 62 }, iPoint(2, 62) });
+	walk_up.frames.push_back({ { 304, 115, 31, 61 }, iPoint(5, 61) });
+	walk_up.frames.push_back({ { 352, 116, 25, 60 }, iPoint(0, 60) });
+	walk_up.speed = 0.2f;
+
+	AddAnimation("walk_up", walk_up);
+
 	Idle = new EnemyFSM::Idle;
 	Walking = new EnemyFSM::Walking;
 
