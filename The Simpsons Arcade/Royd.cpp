00beb96903b80b47cd49ea82f93237e9db2dea49
Joel Lopez <jowie.jowie@gmail.com>
2017-01-09 00:17:14 +0100

    Update Royd spritesheet and sprite positions

--- The Simpsons Arcade/Royd.cpp
+++ The Simpsons Arcade/Royd.cpp
@@ -19,45 +19,45 @@ bool Royd::Start()
 	life = 2;
 
 	SpriteAnimation idle;
-	idle.frames.push_back({ { 16, 272, 33, 64 }, iPoint(4, 64) });
+	idle.frames.push_back({ { 16, 256, 33, 64 }, iPoint(4, 64) });
 
 	AddAnimation("idle", idle);
 
 	SpriteAnimation walk;
-	walk.frames.push_back({ { 16, 32, 20, 64 }, iPoint(0, 64) });
-	walk.frames.push_back({ { 64, 32, 25, 64 }, iPoint(4, 64) });
-	walk.frames.push_back({ { 112, 33, 32, 63 }, iPoint(6, 63) });
-	walk.frames.push_back({ { 160, 33, 24, 63 }, iPoint(3, 63) });
-	walk.frames.push_back({ { 208, 33, 23, 63 }, iPoint(0, 63) });
-	walk.frames.push_back({ { 256, 33, 27, 63 }, iPoint(3, 63) });
-	walk.frames.push_back({ { 304, 34, 32, 62 }, iPoint(8, 62) });
-	walk.frames.push_back({ { 352, 32, 24, 64 }, iPoint(3, 64) });
+	walk.frames.push_back({ { 16, 16, 20, 64 }, iPoint(0, 64) });
+	walk.frames.push_back({ { 64, 16, 25, 64 }, iPoint(4, 64) });
+	walk.frames.push_back({ { 112, 17, 32, 63 }, iPoint(6, 63) });
+	walk.frames.push_back({ { 160, 17, 24, 63 }, iPoint(3, 63) });
+	walk.frames.push_back({ { 208, 17, 23, 63 }, iPoint(0, 63) });
+	walk.frames.push_back({ { 256, 17, 27, 63 }, iPoint(3, 63) });
+	walk.frames.push_back({ { 304, 18, 32, 62 }, iPoint(8, 62) });
+	walk.frames.push_back({ { 352, 16, 24, 64 }, iPoint(3, 64) });
 	walk.speed = 0.2f;
 
 	AddAnimation("walk", walk);
 
 	SpriteAnimation walk_up;
-	walk_up.frames.push_back({ { 16, 114, 25, 62 }, iPoint(0, 62) });
-	walk_up.frames.push_back({ { 64, 114, 27, 62 }, iPoint(0, 62) });
-	walk_up.frames.push_back({ { 112, 114, 32, 62 }, iPoint(1, 62) });
-	walk_up.frames.push_back({ { 160, 116, 27, 60 }, iPoint(0, 60) });
-	walk_up.frames.push_back({ { 208, 115, 26, 61 }, iPoint(0, 61) });
-	walk_up.frames.push_back({ { 256, 114, 28, 62 }, iPoint(2, 62) });
-	walk_up.frames.push_back({ { 304, 115, 31, 61 }, iPoint(5, 61) });
-	walk_up.frames.push_back({ { 352, 116, 25, 60 }, iPoint(0, 60) });
+	walk_up.frames.push_back({ { 16, 98, 25, 62 }, iPoint(0, 62) });
+	walk_up.frames.push_back({ { 64, 98, 27, 62 }, iPoint(0, 62) });
+	walk_up.frames.push_back({ { 112, 98, 32, 62 }, iPoint(1, 62) });
+	walk_up.frames.push_back({ { 160, 100, 27, 60 }, iPoint(0, 60) });
+	walk_up.frames.push_back({ { 208, 99, 26, 61 }, iPoint(0, 61) });
+	walk_up.frames.push_back({ { 256, 98, 28, 62 }, iPoint(2, 62) });
+	walk_up.frames.push_back({ { 304, 99, 31, 61 }, iPoint(5, 61) });
+	walk_up.frames.push_back({ { 352, 100, 25, 60 }, iPoint(0, 60) });
 	walk_up.speed = 0.2f;
 
 	AddAnimation("walk_up", walk_up);
 
 	SpriteAnimation walk_down;
-	walk_down.frames.push_back({ { 16, 192, 29, 64 }, iPoint(8, 64) });
-	walk_down.frames.push_back({ { 64, 193, 25, 63 }, iPoint(4, 63) });
-	walk_down.frames.push_back({ { 112, 194, 23, 62 }, iPoint(2, 61) });
-	walk_down.frames.push_back({ { 160, 193, 24, 63 }, iPoint(3, 63) });
-	walk_down.frames.push_back({ { 208, 192, 23, 64 }, iPoint(2, 64) });
-	walk_down.frames.push_back({ { 256, 193, 25, 63 }, iPoint(6, 63) });
-	walk_down.frames.push_back({ { 304, 194, 30, 62 }, iPoint(7, 62) });
-	walk_down.frames.push_back({ { 352, 193, 39, 63 }, iPoint(11, 63) });
+	walk_down.frames.push_back({ { 16, 176, 29, 64 }, iPoint(8, 64) });
+	walk_down.frames.push_back({ { 64, 177, 25, 63 }, iPoint(4, 63) });
+	walk_down.frames.push_back({ { 112, 178, 23, 62 }, iPoint(2, 61) });
+	walk_down.frames.push_back({ { 160, 177, 24, 63 }, iPoint(3, 63) });
+	walk_down.frames.push_back({ { 208, 176, 23, 64 }, iPoint(2, 64) });
+	walk_down.frames.push_back({ { 256, 177, 25, 63 }, iPoint(6, 63) });
+	walk_down.frames.push_back({ { 304, 178, 30, 62 }, iPoint(7, 62) });
+	walk_down.frames.push_back({ { 352, 177, 39, 63 }, iPoint(11, 63) });
 	walk_down.speed = 0.2f;
 
 	AddAnimation("walk_down", walk_down);
